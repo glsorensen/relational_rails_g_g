e837a5076bc2ab11b000d243c8b23ba71a93a801
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-03 10:17:28 -0500

    Feat: miograte dive database to include timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_000945) do
+ActiveRecord::Schema.define(version: 2022_02_03_012438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dives", force: :cascade do |t|
+    t.string "title"
+    t.integer "location_id"
+    t.boolean "beginner"
+    t.integer "max_depth"
+    t.string "current_strength"
+    t.string "charter_loc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "title"
