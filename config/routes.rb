439b0b53b15d37ec15eda7bb0445914d2e5852d8
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-03 18:03:06 -0500

    Refactor: edit route from plural to singular for model

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/locations/new', to: 'locations#new'
   post '/locations', to: 'locations#create'
   get '/locations/:id', to: 'locations#show'
-  get '/locations/:id/dives', to: 'locations_dives#show_dives' ## PROBABLY WRONG ROUTE
+  get '/locations/:id/dives', to: 'locations_dives#show'
   get '/dive', to: 'dives#index'
   get '/dive/:id', to: 'dives#show'
   get '/teams', to: 'teams#index'
