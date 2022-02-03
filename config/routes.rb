9454c1563f93a86e0029273f987458addb499e78
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-03 10:17:07 -0500

    Feat: create routes for dive index/show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/locations/new', to: 'locations#new'
   post '/locations', to: 'locations#create'
   get '/locations/:id', to: 'locations#show'
+  get '/locations/:id/dives', to: 'locations#show_dives' ## PROBABLY WRONG ROUTE
+  get '/dive', to: 'dives#index'
+  get '/dive/:id', to: 'dives#show'
   get '/teams', to: 'teams#index'
   get '/teams/:id', to: 'teams#show'
 end
