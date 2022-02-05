74bc0911106612ded6f87d88611e1225992162e9
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-05 15:52:23 -0500

    Test: create has a link to all dives for a location test

--- spec/features/locations_dives/show_spec.rb
+++ spec/features/locations_dives/show_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
-RSpec.describe 'dive_locations index page', type: :feature do
+RSpec.describe 'dive_locations page', type: :feature do
   it 'finds location and shows all dives with attributes for that location' do
     location_1 = Location.create!(title: 'Key Largo', has_reefs: true, num_of_species: 600, peak_season: 'June - August', region: 'North America', water_temp: '62F-78F', description: 'Famous for diving')
     dive_1 = Dive.create!(title: "Flahertys Cave",  location_id: location_1.id, beginner: false, max_depth:45, current_strength: "Strong", charter_loc: "Miami, FL")
     dive_2 = Dive.create!(title: "Sorensons Shore", location_id: location_1.id, beginner: false, max_depth:65, current_strength: "Moderate", charter_loc: "Cozumel, MX")
 
     visit "/locations/#{location_1.id}/dives"
-    save_and_open_page
     expect(current_path).to eq("/locations/#{location_1.id}/dives")
     expect(page).to have_content(location_1.title)
     expect(page).to have_content(dive_1.title)
@@ -25,13 +24,16 @@ RSpec.describe 'dive_locations index page', type: :feature do
     expect(page).to have_content(dive_2.updated_at)
   end
 
-  xit 'user story 10' do
-    # User Story 10, Parent Child Index Link
-    #
-    # As a visitor
-    # When I visit a parent show page ('/parents/:id')
-    # Then I see a link to take me to that parent's `child_table_name` page ('/parents/:id/child_table_name')
+  it 'has a link to all dives for a location' do
+    location_1 = Location.create!(title: 'Key Largo', has_reefs: true, num_of_species: 600, peak_season: 'June - August', region: 'North America', water_temp: '62F-78F', description: 'Famous for diving')
+    dive_1 = Dive.create!(title: "Flahertys Cave",  location_id: location_1.id, beginner: false, max_depth:45, current_strength: "Strong", charter_loc: "Miami, FL")
+    dive_2 = Dive.create!(title: "Sorensons Shore", location_id: location_1.id, beginner: false, max_depth:65, current_strength: "Moderate", charter_loc: "Cozumel, MX")
+
+    visit "/locations/#{location_1.id}"
+    save_and_open_page
+    expect(current_path).to eq("/locations/#{location_1.id}")
+    expect(page).to have_content("See All #{location_1.title} Dives")
+    click_link "See All #{location_1.title} Dives"
+    expect(current_path).to eq("/locations/#{location_1.id}/dives")
   end
-
-
 end
